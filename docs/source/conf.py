6c145dd5befb5f8d3332696998e7f869878d6469
proura <pau@brainupdaters.net>
2015-01-21 10:47:18 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -141,28 +141,28 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 
-# html_static_path = ['_static']
+html_static_path = ['_static']
 
 # on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
-# on_rtd = os.environ.get('READTHEDOCS', None) == 'True'                           
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'                           
                                                                                  
-# if not on_rtd:  # only import and set the theme if we're building docs locally   
-#     import sphinx_rtd_theme                                                      
-#     html_theme = 'sphinx_rtd_theme'                                              
-#     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]                   
-#     # Override default css to get a larger width for local build                 
-#     def setup(app):                                                              
-#         #app.add_javascript("custom.js")                                         
-#         app.add_stylesheet('theme_overrides.css')                                
-# else:                                                                            
-#    # Override default css to get a larger width for ReadTheDoc build            
-#     html_context = {                                                             
-#         'css_files': [                                                           
-#             'https://media.readthedocs.org/css/sphinx_rtd_theme.css',            
-#             'https://media.readthedocs.org/css/readthedocs-doc-embed.css',
-#             '_static/css/theme_override.css',
-#         ],                                                                       
-#     }
+if not on_rtd:  # only import and set the theme if we're building docs locally   
+     import sphinx_rtd_theme                                                      
+     html_theme = 'sphinx_rtd_theme'                                              
+     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]                   
+     # Override default css to get a larger width for local build                 
+     def setup(app):                                                              
+         #app.add_javascript("custom.js")                                         
+         app.add_stylesheet('theme_overrides.css')                                
+else:                                                                            
+    # Override default css to get a larger width for ReadTheDoc build            
+     html_context = {                                                             
+         'css_files': [                                                           
+             'https://media.readthedocs.org/css/sphinx_rtd_theme.css',            
+             'https://media.readthedocs.org/css/readthedocs-doc-embed.css',
+             '_static/css/theme_override.css',
+         ],                                                                       
+     }
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
